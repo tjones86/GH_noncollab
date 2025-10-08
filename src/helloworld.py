93a678ceff0108df9038c28ea4194713531981f5
Tiesha Jones <tjones86@gmail.com>
2025-10-07 21:24:56 -0400

    add helloworld.py

---
+++ src/helloworld.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello, World!")  
+    print("This is a simple Python script.")
+    print("It demonstrates basic functionality.")
+    print("Feel free to modify and expand it!")
+    print("Goodbye, World!")
+
+main()
